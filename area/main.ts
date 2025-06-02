0928433092869533c79e73ef197cf2fd4ac92dec
sowmya <sowmya.t@everest.engineering>
2025-06-02 12:08:48 +0530

    Resolve conflicts in division after rebase

--- area/main.ts
+++ area/main.ts
@@ -3,18 +3,18 @@ console.log("challenge 2 third time...")
 import promptSync from 'prompt-sync';
 const prompt = promptSync();
 
-function divideByTwo(value:number): number{
-    return value/2
+function divideByTwo(sum:number): number{
+    return sum/2
 }
 
-const input = Number(prompt('Enter a value to divide by 2: '));
+// const input = Number(prompt('Enter a value to divide by 2: '));
 
-if (isNaN(input)) {
-  console.log('Please enter a valid number.');
-} else {
-  const byTwovalue = divideByTwo(input);
-  console.log(`Output: ${byTwovalue}`);
-}
+// if (isNaN(input)) {
+//   console.log('Please enter a valid number.');
+// } else {
+//   const byTwovalue = divideByTwo(input);
+//   console.log(`by 2 value : ${byTwovalue}`);
+// }
 
 
 function add(a: number, b: number): number {
@@ -27,6 +27,7 @@ const b = Number(prompt("Enter value for b: "));
 if (isNaN(a) || isNaN(b)) {
   console.log("Please enter valid numbers.");
 } else {
-  const result = add(a, b);
-  console.log(`Output: ${result}`);
+  const sum = add(a, b);
+  const byTwovalue = divideByTwo(sum)
+  console.log(`finalValue((a+b)/2): ${byTwovalue}`);
 }
