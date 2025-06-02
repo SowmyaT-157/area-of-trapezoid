f498bc2814db2516bfa91ce82acf9570f0ae925f
sowmya <sowmya.t@everest.engineering>
2025-06-02 11:03:18 +0530

    Add a division functionality

--- area/main.ts
+++ area/main.ts
@@ -1,2 +1,17 @@
 console.log("challenge 2 third time...")
 
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
+
+function divideByTwo(value:number): number{
+    return value/2
+}
+
+const input = Number(prompt('Enter a value to divide by 2: '));
+
+if (isNaN(input)) {
+  console.log('Please enter a valid number.');
+} else {
+  const byTwovalue = divideByTwo(input);
+  console.log(`Output: ${byTwovalue}`);
+}
